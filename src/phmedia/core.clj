2e9fe97b9d83f826c2d1693a54b0111ae0947c72
Alex Korotkov <durm.icecoffee@gmail.com>
2015-01-20 18:51:24 +0300

    get-path

--- src/phmedia/core.clj
+++ src/phmedia/core.clj
@@ -3,20 +3,30 @@
                 [hiccup.middleware :only (wrap-base-url)])
     (:require   [compojure.route :as route]
                 [compojure.handler :as handler]
-                [compojure.response :as response])
+                [compojure.response :as response]
+                [clojure.java.io :as io])
     (:gen-class)
 )
 
 
 (defn uuid [] (str (java.util.UUID/randomUUID)))
 
-(defn phcreate [] (println "create"))
+(defn get-path []
+  (let [filename (uuid)]
+    (->
+      (io/file (subs filename 0 3) (subs filename 3 6) filename)
+      (.getPath))
+    )
+  )
+
+(defn phcreate
+  ([] (println "create")))
 (defn phread [] (println "read"))
 (defn phupdate [] (println "update"))
 (defn phdelete [] (println "delete"))
 
 (defroutes main-routes
-    (GET "/" [] (uuid))
+    (GET "/" [] (get-path))
     (route/not-found "Page not found"))
 
 (def app
