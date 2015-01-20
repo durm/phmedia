952c2160c097febf169125e40deb7fc1265d84f5
Alex Korotkov <durm.icecoffee@gmail.com>
2015-01-20 19:26:19 +0300

    create

--- src/phmedia/core.clj
+++ src/phmedia/core.clj
@@ -1,34 +1,45 @@
 (ns phmedia.core
-    (:use       compojure.core
-                [hiccup.middleware :only (wrap-base-url)])
-    (:require   [compojure.route :as route]
-                [compojure.handler :as handler]
-                [compojure.response :as response]
-                [clojure.java.io :as io])
-    (:gen-class)
-)
+  (:use       compojure.core
+              [hiccup.middleware :only (wrap-base-url)]
+              [hiccup.core])
+  (:require   [compojure.route :as route]
+              [compojure.handler :as handler]
+              [compojure.response :as response]
+              (ring.middleware [multipart-params :as mp])
+              [clojure.java.io :as io])
+  (:gen-class)
+  )
 
 
 (defn uuid [] (str (java.util.UUID/randomUUID)))
 
-(defn get-path []
-  (let [filename (uuid)]
-    (->
-      (io/file (subs filename 0 3) (subs filename 3 6) filename)
-      (.getPath))
-    )
+(defn get-path
+  ([filename] (->
+               (io/file "data" (subs filename 0 3) (subs filename 3 6) filename)
+               (.getPath))
+   )
+  ([] (get-path (uuid)))
   )
 
 (defn phcreate
-  ([] (println "create")))
+  ([tempfile]
+   (let [fid (uuid)]
+     (io/copy tempfile (get-path fid))
+     fid)
+     )
+  )
 (defn phread [] (println "read"))
 (defn phupdate [] (println "update"))
 (defn phdelete [] (println "delete"))
 
 (defroutes main-routes
-    (GET "/" [] (get-path))
-    (route/not-found "Page not found"))
+  (GET "/" [] (get-path))
+  (POST "/" {{{tempfile :tempfile filename :filename} :file} :params :as params}
+        (html [:created {:id (phcreate tempfile)}]))
+  (route/not-found "Page not found"))
 
 (def app
-    (-> (handler/site main-routes)
-    (wrap-base-url)))
+  (-> (handler/site main-routes)
+      (mp/wrap-multipart-params)
+      (wrap-base-url)
+      ))
